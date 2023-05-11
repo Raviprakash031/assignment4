d82b86daffe0bfcf000a43587163da22c30d517f
Ravi Prakash <131772045+RaviPrakash031@users.noreply.github.com>
2023-05-11 09:27:15 +0530

    Update HighestNumber.java

--- HighestNumber.java
+++ HighestNumber.java
@@ -16,20 +16,33 @@
 import java.util.scanner;
 
 public class HighestNumber {
-     
+     public static void main(String[] args){
 //Add main method
 
 //Declare the three variables
+        int number1,number2,number3
+   
 
 //Use the scanner class to provide input at execution time
-/* 
+ 
  Scanner s= new Scanner(System.in);
  System.out.println("Enter the first number");
  number1=s.nextInt();
+ System.out.println("Enter the second number");
+ number2=s.nextInt();
+ System.out.println("Enter the third number");
+ number3=s.nextInt();
  
-*/
+
 
 //check which number is highest using if else condition
+  if (number1>number2 && number1>number3)
+     System.out.println("number1 is higher"+ number1);
+  else if(number2>number1 && number2>number3)
+     System.out.println("number2 is higher"+ number2);
+   else
+       System.out.println("number3 is higher"+ number3);
+      
 
 //Print the highest of three numbers
  
