2e89f7aa5352ad4dca36ac9491070dfe8dfeed70
Ravi Prakash <131772045+RaviPrakash031@users.noreply.github.com>
2023-05-12 09:42:03 +0530

    Update DisplayDay.java

--- DisplayDay.java
+++ DisplayDay.java
@@ -11,21 +11,36 @@
 
 //import statements for java program to read inputs using Scanner class 
 import java.util.Scanner;
-
-public class DisplayDay {
-
-//declare number variable
-
-//Use the scanner class to provide input at execution time using scanner object
-//Scanner s= new Scanner(System.in);
-
-
-/*
-Take input of the number for displaying day
-System.out.println("Enter value for displaying day:");
-number=sc.nextInt();
-*/
-
-//Declare switch case and check the input value to print the day entered by the user
-     
+public class SwitchWeek{
+  public static void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+    int num;
+    System.out.println("enter the week number");
+    num=sc.nextInt();
+    switch(num){
+      case 1:
+        System.out.println("sunday");
+        break;
+      case 2:
+        System.out.println("monday");
+      break;
+      case 3:
+        System.out.println("tuesday");
+      break;
+      case 4:
+        System.out.println("wednesday");
+        break;
+      case 5:
+        System.out.println("thursday");
+      break;
+      case 6:
+        System.out.println("friday");
+      break;
+      case 7:
+        System.out.println("saturday");
+      break;
+      default:
+        System.out.println("invalid week number");
+    }
+  }
 }
